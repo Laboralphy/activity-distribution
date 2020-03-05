fb5676eec9bd844ba87e8ec39eb67be8f410bcae
Raphael Marandet <raphael.marandet@gmail.com>
2020-03-05 21:40:30 +0100

    initial commit

---
+++ src/ActivityDistribution.js
@@ -0,0 +1,238 @@
+/**
+ * Cette classe distribut des activité en font des entrées et des poids
+ */
+class ActivityDistribution {
+
+    constructor() {
+        this._entries = [];
+        this._nSlotCount = 0;
+        this._nEntryCount = 0;
+    }
+
+    get entries() {
+        return this._entries;
+    }
+
+    /**
+     * Defini le nombre d'entry parmis lesquelles il faut distribuer les activités
+     * @param nCount {number}
+     */
+    setEntryCount(nCount) {
+        this._entries = new Array(nCount);
+        for (let i = 0; i < nCount; ++i) {
+            this._entries[i] = {
+                id: null,
+                slots: [],
+                weight: 0,
+                counter: 0
+            };
+        }
+        this._nEntryCount = nCount;
+    }
+
+    setEntries(data, nSlotCount) {
+        this.setEntryCount(data.length);
+        this.setSlotCount(nSlotCount);
+        data.forEach(({id, weight}, iEntry) => {
+            const e = this._entries[iEntry];
+            e.id = id;
+            e.weight = weight;
+        });
+        this.feed((iEntry, iSlot) => {
+            const oDataEntry = data[iEntry];
+            const {busy} = oDataEntry;
+            const value = 0;
+            const enabled = !busy.includes(iSlot);
+            return {
+                value, enabled
+            };
+        });
+    }
+
+    /**
+     * Défini le nombre de slot que chaque entry comporte
+     * @param nCount {number}
+     */
+    setSlotCount(nCount) {
+        for (let i = 0, l = this._entries.length; i < l; ++i) {
+            const slots = [];
+            for (let iSlot = 0; iSlot < nCount; ++iSlot) {
+                slots[iSlot] = {
+                    value: 0,
+                    enabled: true
+                }
+            }
+            this._entries[i].slots = slots;
+        }
+        this._nSlotCount = nCount;
+    }
+
+    getSlotCount() {
+        return this._nSlotCount;
+    }
+
+    getEntryCount() {
+        return this._nEntryCount;
+    }
+
+    /**
+     * Renvoie true si l'entrée dont l'index est spécifié, existe
+     * @param iEntry {number}
+     * @returns {boolean}
+     */
+    isEntryExists(iEntry) {
+        return iEntry >= 0 && iEntry < this._entries.length;
+    }
+
+    /**
+     * Renvoie true si le slot de l'entrée spécifiée, existe
+     * @param iEntry {number} index de l'entrée
+     * @param iSlot {number} index du slot
+     * @returns {boolean}
+     */
+    isSlotExists(iEntry, iSlot) {
+        return this.isEntryExists(iEntry) && iSlot >= 0 && iSlot < this._entries[iEntry].slots.length;
+    }
+
+    /**
+     * Si l'entrée/Slot n'existe pas, une erreur survient
+     * @param iEntry {number} index de l'entrée
+     * @param iSlot {number} index du slot
+     * @throws RangeError
+     */
+    checkSlot(iEntry, iSlot) {
+        if (!this.isSlotExists(iEntry, iSlot)) {
+            throw new RangeError('this entry/slot does not exists : entry:' + iEntry + ' slot:' + iSlot);
+        }
+    }
+
+    getEntrySlot(iEntry, iSlot) {
+        this.checkSlot(iEntry, iSlot);
+        return this._entries[iEntry].slots[iSlot];
+    }
+
+    /**
+     * Active un slot, ce slot pourra etre marké
+     * @param iEntry {number} index de l'entrée
+     * @param iSlot {number} index du slot
+     */
+    enableSlot(iEntry, iSlot) {
+        this.getEntrySlot(iEntry, iSlot).enabled = true;
+    }
+
+    /**
+     * Désctive un slot, ce slot ne pourra plus etre marké
+     * @param iEntry {number} index de l'entrée
+     * @param iSlot {number} index du slot
+     */
+    disableSlot(iEntry, iSlot) {
+        this.getEntrySlot(iEntry, iSlot).enabled = false;
+    }
+
+    /**
+     * Marque le slot spécifié
+     * @param iEntry {number} index de l'entrée
+     * @param iSlot {number} index du slot
+     * @param value {number} nouvelle valuer
+     */
+    setSlotValue(iEntry, iSlot, value) {
+        this.getEntrySlot(iEntry, iSlot).value = value;
+    }
+
+    /**
+     * Efface le slot spécifié
+     * @param iEntry {number} index de l'entrée
+     * @param iSlot {number} index du slot
+     */
+    clearSlot(iEntry, iSlot) {
+        this.getEntrySlot(iEntry, iSlot).value = 0;
+    }
+
+    getSlotValue(iEntry, iSlot) {
+        return this.getEntrySlot(iEntry, iSlot).value;
+    }
+
+    /**
+     * Rempli la grille de slot grace à la fonction spécifé
+     * @param f {function}
+     */
+    feed(f) {
+        const entries = this._entries;
+        for (let iEntry = 0, nEntryCount = entries.length; iEntry < nEntryCount; ++iEntry) {
+            const entry = entries[iEntry];
+            for (let iSlot = 0, nSlotCount = entry.slots.length; iSlot < nSlotCount; ++iSlot) {
+                const slot = entry.slots[iSlot];
+                const {enabled, value} = f(iEntry, iSlot);
+                slot.enabled = enabled;
+                slot.value = value;
+            }
+        }
+    }
+
+    /**
+     * pour l'index de slot donné, renvoie une colone de slot
+     * @param iSlot {number}
+     * @return {Array}
+     */
+    getSlotColumn(iSlot) {
+        const entries = this._entries;
+        const r = [];
+        // pour chaque index de slot
+        for (let iEntry = 0, nEntryCount = this.getEntryCount(); iEntry < nEntryCount; ++iEntry) {
+            const entry = entries[iEntry];
+            const slot = entry.slots[iSlot];
+            if (slot.enabled) {
+                r.push({
+                    entry: iEntry,
+                    counter: entry.counter
+                });
+            }
+        }
+        r.sort((a, b) => b.counter - a.counter);
+        return r;
+    }
+
+    process(value = 1) {
+        const entries = this._entries;
+        // pour chaque index de slot
+        for (let iSlot = 0, nSlotCount = this.getSlotCount(); iSlot < nSlotCount; ++iSlot) {
+            const aColumn = this.getSlotColumn(iSlot);
+            if (aColumn.length > 0) {
+                // pour cette colone de slots il faut déterminer l'entrée qui a un compteur max
+                aColumn.forEach(({entry, counter}, i, aSlots) => {
+                    const iEntry = entry;
+                    const oEntry = this.entries[iEntry];
+                    if (!oEntry) {
+                        throw new Error('undefined entry: ' + iEntry);
+                    }
+                    if (i === 0) {
+                        // l'entrée max
+                        oEntry.counter = 0;
+                        this.setSlotValue(iEntry, iSlot, value);
+                    } else {
+                        // les autres entrées
+                        oEntry.counter += oEntry.weight;
+                    }
+                })
+            } else {
+                // il n'y a pas assez de candidats... tout le monde est disabled
+                throw new Error('could not process')
+            }
+        }
+    }
+
+    render() {
+        const a = [];
+        for (let iEntry = 0; iEntry < this.getEntryCount(); ++iEntry) {
+            const s = [];
+            for (let iSlot = 0; iSlot < this.getSlotCount(); ++iSlot) {
+                const ges = this.getEntrySlot(iEntry, iSlot);
+                s.push(ges.enabled ? ges.value : 'X');
+            }
+            a.push(s.join(' '));
+        }
+        return a.join('\n');
+    }
+}
+
+module.exports = ActivityDistribution;
---
+++ tests/ActivityDistributionTest.js
@@ -0,0 +1,141 @@
+const ActivityDistribution = require('../src/ActivityDistribution');
+
+describe('Primary functions', function() {
+    it('should be instanciable', function() {
+        const ad = new ActivityDistribution();
+        expect(ad).toBeDefined();
+    });
+
+    it('should have 1 entry', function() {
+        const ad = new ActivityDistribution();
+        expect(ad.entries.length).toBe(0);
+        ad.setEntryCount(1);
+        expect(ad.entries.length).toBe(1);
+    });
+
+    it('should have 1 entry and 1 slot', function() {
+        const ad = new ActivityDistribution();
+        ad.setEntryCount(1);
+        ad.setSlotCount(1);
+        expect(ad.entries[0].slots.length).toBe(1);
+    });
+
+    it('set some slot values', function() {
+        const ad = new ActivityDistribution();
+        ad.setEntryCount(3);
+        ad.setSlotCount(6);
+        ad.setSlotValue(0, 0, 1);
+        ad.setSlotValue(2, 5, 1);
+        ad.setSlotValue(1, 2, 1);
+        ad.setSlotValue(1, 0, 1);
+        ad.setSlotValue(0, 4, 1);
+        expect(ad.getEntrySlot(0, 0).value).toBe(1);
+        expect(ad.getEntrySlot(0, 1).value).toBe(0);
+        expect(ad.getEntrySlot(0, 2).value).toBe(0);
+        expect(ad.getEntrySlot(0, 3).value).toBe(0);
+        expect(ad.getEntrySlot(0, 4).value).toBe(1);
+        expect(() => ad.getEntrySlot(0, 14)).toThrow(new RangeError('this entry/slot does not exists : entry:0 slot:14'));
+    })
+});
+
+describe('feed process', function() {
+    it ('should feed ad', function() {
+        const ad = new ActivityDistribution();
+        ad.setEntryCount(3);
+        ad.setSlotCount(3);
+        ad.feed((iEntry, iSlot) => ({
+            enabled: true,
+            value: iEntry * 10 + iSlot
+        }));
+        expect(ad.getSlotValue(0, 0)).toBe(0);
+        expect(ad.getSlotValue(0, 1)).toBe(1);
+        expect(ad.getSlotValue(0, 2)).toBe(2);
+        expect(ad.getSlotValue(1, 0)).toBe(10);
+        expect(ad.getSlotValue(1, 1)).toBe(11);
+        expect(ad.getSlotValue(1, 2)).toBe(12);
+        expect(ad.getSlotValue(2, 0)).toBe(20);
+        expect(ad.getSlotValue(2, 1)).toBe(21);
+        expect(ad.getSlotValue(2, 2)).toBe(22);
+    })
+});
+
+describe('set entries', function() {
+    it ('should affect correct entries', function() {
+        const ad = new ActivityDistribution();
+        ad.setEntries([
+            {
+                id: 1,
+                weight: 2,
+                busy: []
+            },
+            {
+                id: 2,
+                weight: 1,
+                busy: []
+            },
+            {
+                id: 3,
+                weight: 1,
+                busy: []
+            },
+        ], 4);
+        expect(ad.getEntryCount()).toBe(3);
+        expect(ad.getSlotCount()).toBe(4);
+        expect(ad.entries[0].id).toBe(1);
+        expect(ad.entries[1].id).toBe(2);
+        expect(ad.entries[2].id).toBe(3);
+        expect(ad.entries[0].weight).toBe(2);
+        expect(ad.entries[1].weight).toBe(1);
+        expect(ad.entries[2].weight).toBe(1);
+    });
+});
+
+describe('process', function() {
+    it ('should process', function() {
+        const ad = new ActivityDistribution();
+        ad.setEntries([
+            {
+                id: 1,
+                weight: 2,
+                busy: []
+            },
+            {
+                id: 2,
+                weight: 1,
+                busy: []
+            },
+            {
+                id: 3,
+                weight: 1,
+                busy: []
+            },
+        ], 4);
+        ad.process(1);
+        expect(ad.render()).toBe('1 0 1 0\n0 1 0 0\n0 0 0 1');
+    });
+});
+
+describe('process with busy', function() {
+    it ('should process', function() {
+        const ad = new ActivityDistribution();
+        ad.setEntries([
+            {
+                id: 1,
+                weight: 2,
+                busy: [0, 1, 2, 3, 4]
+            },
+            {
+                id: 2,
+                weight: 1,
+                busy: [5, 9]
+            },
+            {
+                id: 3,
+                weight: 1,
+                busy: [8, 9, 10, 11]
+            },
+        ], 12);
+        ad.process(1);
+        console.log('\n' + ad.render());
+    });
+});
